23cabe91cef1bc2500fa92f45cc3f321deaccd3b
Joe Davis <ElwoodMoves@hotmail.com>
2024-04-04 22:36:29 -0700

    Adding a SpeechController that does nothing except initialize the Listening view model with a message.

---
+++ AdaptiveRemote/Services/IRemoteDefinitionServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using AdaptiveRemote.Models;
+
+namespace AdaptiveRemote.Services;
+
+internal static class IRemoteDefinitionServiceExtensions
+{
+    internal static IEnumerable<Command> GetCommands(this IRemoteDefinitionService service, string remoteName)
+    {
+        return service.GetAllElements(remoteName).OfType<Command>();
+    }
+
+    internal static ElementType GetElement<ElementType>(this IRemoteDefinitionService service, string remoteName)
+    {
+        return service.GetAllElements(remoteName).OfType<ElementType>().First();
+    }
+
+    private static IEnumerable<RemoteLayoutElement> GetAllElements(this IRemoteDefinitionService service, string remoteName)
+    {
+        Stack<IEnumerator<RemoteLayoutElement>> stack = new();
+        IEnumerator<RemoteLayoutElement> currentIter = new[] { service.GetRoot(remoteName) }.AsEnumerable().GetEnumerator();
+
+        while (true)
+        {
+            if (currentIter.MoveNext())
+            {
+                yield return currentIter.Current;
+
+                if (currentIter.Current is LayoutGroup group)
+                {
+                    stack.Push(currentIter);
+                    currentIter = group.Elements.GetEnumerator();
+                }
+            }
+            else if (stack.Count > 0)
+            {
+                currentIter = stack.Pop();
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ AdaptiveRemote/Services/Speech/SpeechController.cs
@@ -0,0 +1,31 @@
+﻿using AdaptiveRemote.Models;
+using Microsoft.Extensions.Logging;
+
+namespace AdaptiveRemote.Services.Speech;
+
+internal class SpeechController : ISpeechController, IDisposable
+{
+    private readonly ILogger<SpeechController> _logger;
+    private readonly Listening _viewModel;
+
+    public SpeechController(ILogger<SpeechController> logger, Listening viewModel)
+    {
+        _logger = logger;
+        _viewModel = viewModel;
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+        //_logger.LogInformation("Started");
+        //_viewModel.StatusMessage = "Say \"Hey Remote\" to start voice commands";
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+        //_logger.LogInformation("Stopped");
+        //_viewModel.StatusMessage = String.Empty;
+        //_viewModel.IsListening = false;
+    }
+}
