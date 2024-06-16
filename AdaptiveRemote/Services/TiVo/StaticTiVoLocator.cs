1ed6990f1f95be9f8ddd4be7889641f0c9454b4f
Joe Davis <ElwoodMoves@hotmail.com>
2024-06-16 07:37:23 -0700

    531 Implement StaticTiVoLocator

---
+++ AdaptiveRemote/Services/TiVo/StaticTiVoLocator.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Net;
+using Microsoft.Extensions.Options;
+
+namespace AdaptiveRemote.Services.TiVo;
+
+internal class StaticTiVoLocator : ITiVoLocator
+{
+    private readonly TiVoSettings _settings;
+
+    public StaticTiVoLocator(IOptionsSnapshot<TiVoSettings> settings)
+    {
+        _settings = settings.Value;
+    }
+
+    Task<EndPoint> ITiVoLocator.FindTiVoAsync(CancellationToken cancellationToken)
+    {
+        string ipString = _settings.StaticIP
+            ?? throw new ConfigurationErrorsException("The StaticIP setting is required to connect to the TiVo");
+
+        EndPoint endpoint = IPEndPoint.Parse(ipString);
+
+        return Task.FromResult(endpoint);
+    }
+}
