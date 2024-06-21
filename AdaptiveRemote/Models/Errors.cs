21763f6f010dbc96de7804eae88a88b773dd811c
Joe Davis <ElwoodMoves@hotmail.com>
2024-06-21 06:47:40 -0700

    Move user-facing error messages to a common file.

---
+++ AdaptiveRemote/Models/Errors.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace AdaptiveRemote.Models;
+
+internal static class Errors
+{
+    public static Exception Telemetry_ConnectionStringRequired(string settingKey, string settingName)
+        => new ConfigurationErrorsException($"The '{settingKey}:{settingName}' setting is required to publish telemetry");
+    public static Exception Commands_UnsupportedCommandType(Command command, string argumentName)
+        => new ArgumentException($"Cannot execute {command}", argumentName);
+}
