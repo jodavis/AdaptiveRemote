6966e97e5f1a84fe6bc8e9fbe683115ed4d391dd
Joe Davis <ElwoodMoves@hotmail.com>
2024-06-21 10:50:30 -0700

    Created a AdaptiveRemote.Console host program that allows me to monitor the standard output of the application for debugging purposes.

---
+++ AdaptiveRemote.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.Windows;
+using Microsoft.Extensions.Hosting;
+
+namespace AdaptiveRemote;
+
+internal class Program : Application
+{
+    private readonly IHost _host;
+
+    public Program(string[] args)
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureAppSettings(args)
+            .ConfigureApp()
+            .Build();
+    }
+
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        new Program(args).Run();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _ = StartApplicationLoopAsync();
+    }
+
+    private async Task StartApplicationLoopAsync()
+    {
+        try
+        {
+            await _host.RunAsync();
+        }
+        catch (ConfigurationErrorsException configErrors)
+        {
+            Console.WriteLine(configErrors.Message);
+        }
+        finally
+        {
+            Shutdown();
+        }
+    }
+}
