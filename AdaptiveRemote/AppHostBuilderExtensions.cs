3ef2929208f44e2d3b6d12f688c516d19d6cb240
Joe Davis <ElwoodMoves@hotmail.com>
2024-06-21 07:08:12 -0700

    Improve the default "publishing" behavior so that the application publishes data by default, but E2E tests could initialize a host application wihtout publishing data.

---
+++ AdaptiveRemote/AppHostBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using AdaptiveRemote.Services.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AdaptiveRemote;
+
+internal static class AppHostBuilderExtensions
+{
+    internal static IHostBuilder ConfigureApp(this IHostBuilder hostBuilder)
+        => hostBuilder
+            .AddBlazorUI()
+            .ConfigureTelemetry()
+            .AddRemoteServices()
+            .AddConversationSystem();
+
+    internal static IHostBuilder ConfigureAppSettings(this IHostBuilder hostBuilder, string[] args)
+        => hostBuilder
+            .ConfigureAppConfiguration(config =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    ["telemetry:Publish"] = "True"
+                });
+                config.AddUserSecrets<App>();
+                config.AddCommandLine(args);
+            });
+
+    private static IHostBuilder AddBlazorUI(this IHostBuilder hostBuilder)
+        => hostBuilder.ConfigureServices(services => services
+            .AddSingleton<MainWindow>()
+            .AddWpfBlazorWebView());
+}
