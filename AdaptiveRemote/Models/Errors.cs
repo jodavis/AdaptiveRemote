8044e9891badc9de02d0f8302813bbb4d0acda32
Joe Davis <ElwoodMoves@hotmail.com>
2024-06-22 14:50:12 -0700

    Move configuration keys to a common location.

--- AdaptiveRemote/Models/Errors.cs
+++ AdaptiveRemote/Models/Errors.cs
@@ -1,17 +1,18 @@
 ﻿using System.Configuration;
+using AdaptiveRemote.Services.Configuration;
 
 namespace AdaptiveRemote.Models;
 
 internal static class Errors
 {
-    public static Exception Telemetry_ConnectionStringRequired(string settingKey, string settingName)
-        => new ConfigurationErrorsException($"The '{settingKey}:{settingName}' setting is required to publish telemetry");
+    public static Exception Telemetry_ConnectionStringRequired(string settingName)
+        => new ConfigurationErrorsException($"The '{SettingsKeys.Telemetry}:{settingName}' setting is required to publish telemetry");
 
     public static Exception Commands_UnsupportedCommandType(Command command, string argumentName)
         => new ArgumentException($"Cannot execute {command}", argumentName);
 
-    internal static Exception TiVo_IPAddressRequired(string settingKey, string settingName)
-        => new ConfigurationErrorsException($"The '{settingKey}:{settingName}' setting is required to connect to the TiVo");
+    internal static Exception TiVo_IPAddressRequired(string settingName)
+        => new ConfigurationErrorsException($"The '{SettingsKeys.TiVo}:{settingName}' setting is required to connect to the TiVo");
     internal static Exception TiVo_CannotInterpretCommand(string commandId, string argName)
         => new ArgumentException($"Unable to interpret '{commandId}' as a TiVo command", argName);
     internal static Exception TiVo_NotInitialized(string command)
