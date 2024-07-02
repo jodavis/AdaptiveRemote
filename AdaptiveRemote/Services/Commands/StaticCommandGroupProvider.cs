7bac85b9df791cff7305c1b628b7b0a4c77b484d
Joe Davis <ElwoodMoves@hotmail.com>
2024-07-02 08:38:15 -0700

    Add volume control buttons to the UI

--- AdaptiveRemote/Services/Commands/StaticCommandGroupProvider.cs
+++ AdaptiveRemote/Services/Commands/StaticCommandGroupProvider.cs
@@ -4,40 +4,43 @@ namespace AdaptiveRemote.Services.Commands;
 
 internal class StaticCommandGroupProvider : IRemoteDefinitionService
 {
-    public RemoteLayoutElement RemoteRoot { get; } = new LayoutGroup("ROOT", new List<RemoteLayoutElement>
-    {
-        new LayoutGroup("DPAD", new List<RemoteLayoutElement>
-        {
+    public RemoteLayoutElement RemoteRoot { get; } = new LayoutGroup("ROOT",
+    [
+        new LayoutGroup("DPAD",
+        [
             new TiVoCommand("Up"),
             new TiVoCommand("Down"),
             new TiVoCommand("Left"),
             new TiVoCommand("Right"),
             new TiVoCommand("Select"),
             new TiVoCommand("Back"),
-        }, "ROOT"),
-        new LayoutGroup("WELL", new List<RemoteLayoutElement>
-        {
+        ]),
+        new LayoutGroup("WELL",
+        [
             new TiVoCommand("TiVo"),
             new TiVoCommand("Netflix"),
             new TiVoCommand("Guide"),
-        }, "ROOT"),
-        new LayoutGroup("PLAYBACK", new List<RemoteLayoutElement>
-        {
+        ]),
+        new LayoutGroup("PLAYBACK",
+        [
             new TiVoCommand("Play"),
             new TiVoCommand("Pause"),
             new TiVoCommand("Record"),
             new TiVoCommand("Skip"),
             new TiVoCommand("Replay"),
-        }, "ROOT"),
-        new LayoutGroup("CHANNEL", new List<RemoteLayoutElement>
-        {
+        ]),
+        new LayoutGroup("CHANNELANDVOLUME",
+        [
             new TiVoCommand("ChannelUp", label: "Up"),
             new TiVoCommand("ChannelDown", label: "Down"),
-        }, "ROOT"),
-        new LayoutGroup("GUTTER", new List<RemoteLayoutElement>
-        {
+            new IRCommand("VolumeUp", label: "Up", data: "JgBQAAABHpEQExETETcQFBATERMQFBETEDcRNxATEDcSNhE3EDcQNxEUEDcQExETEBQQExEUEBMRNxATEDcSNxA3EDcQNxE3EQAFBwABH0gQAA0F"),
+            new IRCommand("VolumeDown", label: "Down", data: "JgBQAAABHpIQExETEDcQFBESEhMQExETEDcQNxITEDcQNxE2EjcQNxA3EDcRExETERMQExETEBQRExAUEDcQNxA3EjYRNxA3EAAFCAABH0gQAA0F"),
+            new IRCommand("Mute", label: "Mute", data: "JgBQAAABH5ERExATETcQExEUEBMRExAUEDcRNxATETcQNxE3EDcQNxA3ERMRFBA3EBMRExATEhMQFBA3EDcQFBE3EDcQNxA3EQAFBwABH0gRAA0F"),
+        ]),
+        new LayoutGroup("GUTTER",
+        [
             new Models.ConversationView(),
             new ApplicationCommand("Exit")
-        }, "ROOT")
-    }, string.Empty);
+        ])
+    ]);
 }
