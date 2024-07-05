e8cd89c3ca002fb653bbd4f6d92fd7769726aea6
Joe Davis <ElwoodMoves@hotmail.com>
2024-07-05 07:47:59 -0700

    Implement the happy path of UdpService.SendAsync

---
+++ AdaptiveRemote.Tests/Services/Broadlink/UdpServiceTests.cs
@@ -0,0 +1,141 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using AdaptiveRemote.TestUtilities;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace AdaptiveRemote.Services.Broadlink;
+
+[TestClass]
+public class UdpServiceTests
+{
+    private readonly Mock<ISocketFactory> MockSocketFactory = new();
+    private readonly Mock<ISocket> MockSocket = new();
+    private readonly BroadlinkSettings Settings = new();
+    private readonly MockLogger<UdpService> MockLogger = new();
+
+    public TestContext? TestContext { get; set; }
+
+    [TestInitialize]
+    public void SetupMocks()
+    {
+        MockSocketFactory
+            .Setup(x => x.Create())
+            .Verifiable(Times.Never);
+        MockSocketFactory
+            .Setup(x => x.CreateForBroadcast())
+            .Verifiable(Times.Never);
+    }
+
+    [TestCleanup]
+    public void VerifyMocks()
+    {
+        MockSocketFactory.Verify();
+        MockSocket.Verify();
+    }
+
+    private UdpService CreateSut()
+    {
+        Mock<IOptions<BroadlinkSettings>> mockOptions = new();
+        mockOptions
+            .SetupGet(x => x.Value)
+            .Returns(Settings);
+
+        return new(MockSocketFactory.Object, mockOptions.Object, MockLogger);
+    }
+
+    [TestMethod]
+    public void UdpService_SendAsync_CreatesSocketSendsPacketAndReturnsResponse()
+    {
+        // Arrange
+        IUdpService sut = CreateSut();
+
+        EndPoint inputEndPoint = IPEndPoint.Parse("192.168.10.20:4321");
+        SendPacket inputPacket = new(
+            new()
+            {
+                DeviceID = 1,
+                DeviceType = 2,
+                HostAddress = new PhysicalAddress([0x12, 0x23, 0x34, 0x45, 0x56, 0x67]),
+                MessageCount = 3,
+                PacketChecksum = 4,
+                PacketType = 5,
+                PayloadChecksum = 6,
+            },
+            new(new byte[] { 0x01, 0x23, 0x45, 0x67 }));
+        ResponsePacket expectedResponse = new(
+            inputEndPoint,
+            new byte[]
+            {
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0xAB, 0xBC, 0xCD, 0xDE, 0xEF, 0xED, 0xDC, 0xBA,
+            });
+
+        Expect_SocketFactory_Create();
+        Expect_Socket_SetTimeout(Settings.SendTimeout);
+        Expect_Socket_SendToAsync(inputPacket.GetBuffer());
+        Expect_Socket_ReadFromAsync(expectedResponse.GetBuffer());
+
+        // Act
+        Task<ResponsePacket> resultTask = sut.SendAsync(inputEndPoint, inputPacket, default);
+
+        // Assert
+        TaskAssert.IsComplete(resultTask, nameof(resultTask));
+
+        ResponsePacket result = resultTask.Result;
+        Assert.IsNotNull(result, nameof(result));
+
+        MemoryAssert.WriteTo(TestContext, nameof(result) + ".GetBuffer()", expectedResponse.GetBuffer(), result.GetBuffer());
+        MemoryAssert.AreEqual(expectedResponse.GetBuffer(), result.GetBuffer(), nameof(result) + ".GetBuffer()");
+
+        Assert.AreEqual(inputEndPoint, result.RemoteEndPoint, nameof(result.RemoteEndPoint));
+    }
+
+    private void Expect_SocketFactory_Create()
+        => MockSocketFactory
+            .Setup(x => x.Create())
+            .Returns(MockSocket.Object)
+            .Verifiable(Times.Once);
+
+    private void Expect_Socket_SetTimeout(int expectedTimeout)
+        => MockSocket
+            .Setup(x => x.SetTimeout(It.IsAny<TimeSpan>()))
+            .WithArgumentValidation("timeout", TimeSpan.FromSeconds(expectedTimeout))
+            .Verifiable(Times.Once);
+
+    private void Expect_Socket_SendToAsync(ReadOnlyMemory<byte> expectedBytes)
+        => MockSocket
+            .Setup(x => x.SendToAsync(It.IsAny<ReadOnlyMemory<byte>>(), It.IsAny<EndPoint>(), It.IsAny<CancellationToken>()))
+            .WithArgumentValidation("packet", delegate (ReadOnlyMemory<byte> actualBytes)
+            {
+                MemoryAssert.WriteTo(TestContext, "packet for SendToAsync", expectedBytes, actualBytes);
+                MemoryAssert.AreEqual(expectedBytes, actualBytes, "packet for SendToAsync");
+
+                Assert.IsTrue(actualBytes.Span.SequenceEqual(expectedBytes.Span));
+            })
+            .WithStandardTaskBehavior(returnValue: expectedBytes.Length)
+            .Verifiable(Times.Once);
+
+    private void Expect_Socket_ReadFromAsync(ReadOnlyMemory<byte> responseBytes)
+        => MockSocket
+            .Setup(x => x.ReceiveFromAsync(It.IsAny<Memory<byte>>(), It.IsAny<EndPoint>(), It.IsAny<CancellationToken>()))
+            .WithArgumentValidation("buffer", delegate (Memory<byte> responseBuffer)
+            {
+                Assert.IsNotNull(responseBuffer, nameof(responseBuffer));
+                Assert.IsTrue(responseBuffer.Length >= responseBytes.Length, "Response buffer for {0} must be at least {1} bytes, but was only {2} bytes",
+                    nameof(ISocket.ReceiveFromAsync),
+                    responseBytes.Length,
+                    responseBuffer.Length);
+
+                responseBytes.CopyTo(responseBuffer);
+            })
+            .WithStandardTaskBehavior(new SocketReceiveFromResult() { ReceivedBytes = responseBytes.Length })
+            .Verifiable(Times.Once);
+}
--- AdaptiveRemote.Tests/TestUtilities/MemoryAssert.cs
+++ AdaptiveRemote.Tests/TestUtilities/MemoryAssert.cs
@@ -5,42 +5,50 @@ namespace AdaptiveRemote.TestUtilities;
 
 internal static class MemoryAssert
 {
-    internal static void AreEqual(Memory<byte> expected, Memory<byte> actual)
+    internal static void AreEqual(ReadOnlyMemory<byte> expected, ReadOnlyMemory<byte> actual, string name)
     {
-        Span<byte>.Enumerator expectedIter = expected.Span.GetEnumerator();
-        Span<byte>.Enumerator actualIter = actual.Span.GetEnumerator();
+        ReadOnlySpan<byte>.Enumerator expectedIter = expected.Span.GetEnumerator();
+        ReadOnlySpan<byte>.Enumerator actualIter = actual.Span.GetEnumerator();
 
         int index = 0;
         while (expectedIter.MoveNext())
         {
             if (!actualIter.MoveNext())
             {
-                Assert.AreEqual(expected.Length, actual.Length, "Number of bytes");
+                Assert.AreEqual(expected.Length, actual.Length, "Number of bytes in {0}", name);
                 Assert.Fail("Same number of bytes were expected, but {0}.MoveNext() returned false", nameof(actualIter));
             }
 
-            Assert.AreEqual(expectedIter.Current.ToString("X2"), actualIter.Current.ToString("X2"), "actual[{0}]", index);
+            Assert.AreEqual(expectedIter.Current.ToString("X2"), actualIter.Current.ToString("X2"), "{1}}[{0}]", index, name);
 
             index++;
         }
 
         if (actualIter.MoveNext())
         {
-            Assert.AreEqual(expected.Length, actual.Length, "Number of bytes");
+            Assert.AreEqual(expected.Length, actual.Length, "Number of bytes in {0}", name);
             Assert.Fail("Same number of bytes were expected, but {0}.MoveNext() returned true", nameof(actualIter));
         }
     }
 
-    internal static void WriteTo(TestContext? testContext, Memory<byte> expected, Memory<byte> actual)
+    internal static void WriteTo(TestContext? testContext, ReadOnlyMemory<byte> expected, ReadOnlyMemory<byte> actual)
+        => WriteTo(testContext, string.Empty, expected, actual);
+
+    internal static void WriteTo(TestContext? testContext, string description, ReadOnlyMemory<byte> expected, ReadOnlyMemory<byte> actual)
     {
         if (testContext is null)
         {
             return;
         }
 
-        Span<byte>.Enumerator expectedIter = expected.Span.GetEnumerator();
-        Span<byte>.Enumerator actualIter = actual.Span.GetEnumerator();
+        ReadOnlySpan<byte>.Enumerator expectedIter = expected.Span.GetEnumerator();
+        ReadOnlySpan<byte>.Enumerator actualIter = actual.Span.GetEnumerator();
 
+        if (description.Length > 0)
+        {
+            testContext.WriteLine("");
+            testContext.WriteLine(description);
+        }
         testContext.WriteLine("Exp\tActual");
 
         while (expectedIter.MoveNext())
