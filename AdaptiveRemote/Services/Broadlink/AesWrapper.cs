c7583a74d48001e06f0365218a0da6adcd1b2aee
Joe Davis <ElwoodMoves@hotmail.com>
2024-07-13 15:54:06 -0700

    Invert the Factory/Object relationship to make features easier to find

---
+++ AdaptiveRemote/Services/Broadlink/AesWrapper.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace AdaptiveRemote.Services.Broadlink;
+
+internal class AesWrapper : IEncryption
+{
+    private static readonly byte[] InitialKey = [0x09, 0x76, 0x28, 0x34, 0x3f, 0xe9, 0x9e, 0x23, 0x76, 0x5c, 0x15, 0x13, 0xac, 0xcf, 0x8b, 0x02];
+    private static readonly byte[] InitialVector = [0x56, 0x2e, 0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58];
+    private static readonly byte[] PaddingBytes = new byte[15];
+
+    private static readonly IEncryption _default = new AesWrapper(InitialKey);
+
+    private readonly Aes _aes;
+
+    private AesWrapper(byte[] key)
+    {
+        _aes = Aes.Create();
+        _aes.KeySize = 256;
+        _aes.BlockSize = 128;
+        _aes.Mode = CipherMode.CBC;
+        _aes.IV = InitialVector;
+        _aes.Key = key;
+        _aes.Padding = PaddingMode.None;
+    }
+
+    Memory<byte> IEncryption.Encrypt(Memory<byte> input)
+    {
+        using ICryptoTransform encryptor = _aes.CreateEncryptor();
+        using MemoryStream toStream = new MemoryStream();
+        using CryptoStream fromStream = new CryptoStream(toStream, encryptor, CryptoStreamMode.Write);
+
+        fromStream.Write(input.Span);
+        int padding = (8192 - input.Length) % 16;
+        if (padding > 0)
+        {
+            fromStream.Write(PaddingBytes, 0, padding);
+        }
+
+        return toStream.ToArray();
+    }
+
+    Memory<byte> IEncryption.Decrypt(Memory<byte> input)
+    {
+        using ICryptoTransform decryptor = _aes.CreateDecryptor();
+        using MemoryStream fromStream = new MemoryStream(input.ToArray());
+        using CryptoStream toStream = new CryptoStream(fromStream, decryptor, CryptoStreamMode.Read);
+        using MemoryStream resultStream = new MemoryStream();
+
+        toStream.CopyTo(resultStream);
+
+        return resultStream.ToArray();
+    }
+
+    internal class Factory : IEncryptionFactory
+    {
+        IEncryption IEncryptionFactory.Default => _default;
+
+        IEncryption IEncryptionFactory.Create(byte[] encryptionKey) => new AesWrapper(encryptionKey);
+
+    }
+}
---
+++ AdaptiveRemote/Services/Broadlink/SocketWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AdaptiveRemote.Services.Broadlink;
+
+[ExcludeFromCodeCoverage(Justification = "Simple wrapper around System.Net.Socket")]
+internal class SocketWrapper : ISocket
+{
+    private Socket _socket;
+
+    private SocketWrapper(Socket socket)
+    {
+        _socket = socket;
+    }
+
+    ValueTask<int> ISocket.SendToAsync(ReadOnlyMemory<byte> packet, EndPoint endPoint, CancellationToken cancellationToken)
+        => _socket.SendToAsync(packet, endPoint, cancellationToken);
+    ValueTask<int> ISocket.ReceiveAsync(Memory<byte> buffer, CancellationToken cancellationToken)
+        => _socket.ReceiveAsync(buffer, cancellationToken);
+    ValueTask<SocketReceiveFromResult> ISocket.ReceiveFromAsync(Memory<byte> buffer, EndPoint remoteEP, CancellationToken cancellationToken)
+        => _socket.ReceiveFromAsync(buffer, remoteEP, cancellationToken);
+
+    void ISocket.SetTimeout(TimeSpan time_left)
+    {
+        _socket.SendTimeout = (int)time_left.TotalMilliseconds;
+        _socket.ReceiveTimeout = (int)time_left.TotalMilliseconds;
+    }
+
+    void IDisposable.Dispose()
+    {
+        _socket.Close();
+        _socket.Dispose();
+    }
+
+    internal class Factory : ISocketFactory
+    {
+        ISocket ISocketFactory.CreateForBroadcast()
+        {
+            Socket socket = CreateUdpSocket();
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+            socket.Bind(new IPEndPoint(0, 0));
+
+            return new SocketWrapper(socket);
+        }
+
+        ISocket ISocketFactory.Create() => new SocketWrapper(CreateUdpSocket());
+
+        private static Socket CreateUdpSocket() => new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    }
+}
