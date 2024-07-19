251dbfcb63610e2661c61a763cf3e37d918e69a8
Joe Davis <ElwoodMoves@hotmail.com>
2024-07-18 22:16:52 -0700

    Doc comments for settings.

--- AdaptiveRemote/Services/Broadlink/BroadlinkSettings.cs
+++ AdaptiveRemote/Services/Broadlink/BroadlinkSettings.cs
@@ -2,6 +2,14 @@
 
 public class BroadlinkSettings
 {
+    /// <summary>
+    /// Amount of time, in seconds, that we will wait for a response to sending data.
+    /// </summary>
     public int SendTimeout { get; set; } = 5;
+
+    /// <summary>
+    /// Amoutn of time, in seconds, that we will wait for a response when scanning
+    /// for devices.
+    /// </summary>
     public double ScanTimeout { get; set; } = 3;
 }
