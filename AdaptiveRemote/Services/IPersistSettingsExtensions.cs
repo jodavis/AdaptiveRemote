03aefad5ab9394cc4d9647db6ced81986b7668c6
Joe Davis <ElwoodMoves@hotmail.com>
2024-07-28 17:23:45 -0700

    Scaffolding for the TiVo locator

---
+++ AdaptiveRemote/Services/IPersistSettingsExtensions.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AdaptiveRemote.Services;
+
+internal static class IPersistSettingsExtensions
+{
+    public static void Set(this IPersistSettings persistSettings, string[] nameComponents, object? typedValue)
+        => persistSettings.Set(Name(nameComponents), Value(typedValue));
+    public static void Set(this IPersistSettings persistSettings, string name, object? typedValue)
+        => persistSettings.Set(name, Value(typedValue));
+    public static void Set(this IPersistSettings persistSettings, string[] nameComponents, string value)
+        => persistSettings.Set(Name(nameComponents), value);
+
+    private static string Name(string[] nameComponents) => throw new NotImplementedException();
+    private static string Value(object? typedValue) => throw new NotImplementedException();
+}
--- AdaptiveRemote/Services/TiVo/TiVoSettings.cs
+++ AdaptiveRemote/Services/TiVo/TiVoSettings.cs
@@ -3,5 +3,6 @@ namespace AdaptiveRemote.Services.TiVo;
 
 public class TiVoSettings
 {
+    // TODO: Remove this when ScanningTiVoLocator isn't using it anymore
     public string? IP { get; set; }
 }
