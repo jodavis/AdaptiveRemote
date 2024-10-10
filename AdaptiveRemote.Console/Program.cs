c21f9b400d7cba75fb2536efea8836e847047f0f
Joe Davis <ElwoodMoves@hotmail.com>
2024-10-10 07:19:39 -0700

    Move more shared logic into the real application by deriving the Console.Program class from the WPF App class.

--- AdaptiveRemote.Console/Program.cs
+++ AdaptiveRemote.Console/Program.cs
@@ -1,39 +1,15 @@
-﻿using System.Windows;
-using AdaptiveRemote.Services.Lifecycle;
+﻿namespace AdaptiveRemote;
 
-namespace AdaptiveRemote;
-
-internal class Program : Application
+internal class Program : App
 {
-    private readonly AcceleratedServices _accelerator;
-
     public Program(string[] args)
+        : base(args)
     {
-        _accelerator = new(args);
     }
 
     [STAThread]
     private static void Main(string[] args)
     {
         new Program(args).Run();
-    }
-
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-
-        _ = StartApplicationLoopAsync();
-    }
-
-    private async Task StartApplicationLoopAsync()
-    {
-        try
-        {
-            await _accelerator.StartApplicationLoopAsync();
-        }
-        finally
-        {
-            Dispatcher.Invoke(Shutdown);
-        }
     }
 }
