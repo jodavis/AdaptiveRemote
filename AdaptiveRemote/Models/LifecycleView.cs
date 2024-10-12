a6fa1e7e3ec304ddaafa50c7cada9e55a3f35d46
Joe Davis <ElwoodMoves@hotmail.com>
2024-10-12 12:55:07 -0700

    Move MainWindow logic to the LifecycleView/Controller so it can be unit tested independently of the UI.
    Also refactored the XAML so it's easier to share properties and read.

--- AdaptiveRemote/Models/LifecycleView.cs
+++ AdaptiveRemote/Models/LifecycleView.cs
@@ -1,12 +1,20 @@
-﻿using AdaptiveRemote.Mvvm;
+﻿using System.Windows.Input;
+using AdaptiveRemote.Mvvm;
 
 namespace AdaptiveRemote.Models;
 
 public class LifecycleView : MvvmObject
 {
     public static readonly MvvmProperty<string> TaskNameProperty = new(nameof(TaskName));
-    public static readonly MvvmProperty<Exception?> FatalErrorProperty = new(nameof(FatalError));
+    public static readonly MvvmProperty<string?> FatalErrorProperty = new(nameof(FatalError));
+    public static readonly MvvmProperty<string?> FatalErrorDetailProperty = new(nameof(FatalErrorDetail));
     public static readonly MvvmProperty<LifecyclePhase> CurrentPhaseProperty = new(nameof(CurrentPhase));
+    public static readonly MvvmProperty<bool> ShowErrorProperty = new(nameof(ShowError));
+    public static readonly MvvmProperty<bool> ShowBrowserProperty = new(nameof(ShowBrowser), defaultValue: true);
+    public static readonly MvvmProperty<bool> ShowButtonPanelProperty = new(nameof(ShowButtonPanel));
+    public static readonly MvvmProperty<ICommand> ShutdownCommandProperty = new(nameof(ShutdownCommand));
+    public static readonly MvvmProperty<ICommand> ShowErrorCommandProperty = new(nameof(ShowErrorCommand));
+    public static readonly MvvmProperty<ICommand> CloseErrorCommandProperty = new(nameof(CloseErrorCommand));
 
     public string TaskName
     {
@@ -14,15 +22,57 @@ public class LifecycleView : MvvmObject
         set => SetValue(TaskNameProperty, value);
     }
 
-    public Exception? FatalError
+    public string? FatalError
     {
         get => GetValue(FatalErrorProperty);
         set => SetValue(FatalErrorProperty, value);
+    }
+
+    public string? FatalErrorDetail
+    {
+        get => GetValue(FatalErrorDetailProperty);
+        set => SetValue(FatalErrorDetailProperty, value);
     }
 
     public LifecyclePhase CurrentPhase
     {
         get => GetValue(CurrentPhaseProperty);
         set => SetValue(CurrentPhaseProperty, value);
+    }
+
+    public bool ShowError
+    {
+        get => GetValue(ShowErrorProperty);
+        set => SetValue(ShowErrorProperty, value);
+    }
+
+    public bool ShowBrowser
+    {
+        get => GetValue(ShowBrowserProperty);
+        set => SetValue(ShowBrowserProperty, value);
+    }
+
+    public bool ShowButtonPanel
+    {
+        get => GetValue(ShowButtonPanelProperty);
+        set => SetValue(ShowButtonPanelProperty, value);
+    }
+
+    public ICommand ShutdownCommand
+    {
+        get => GetValue(ShutdownCommandProperty);
+        set => SetValue(ShutdownCommandProperty, value);
+    }
+
+    public ICommand ShowErrorCommand
+    {
+        get => GetValue(ShowErrorCommandProperty);
+        set => SetValue(ShowErrorCommandProperty, value);
+    }
+
+    public ICommand CloseErrorCommand
+    {
+        get => GetValue(CloseErrorCommandProperty);
+        set => SetValue(CloseErrorCommandProperty, value);
     }
 }
